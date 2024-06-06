b34cf0e5e1ede07848336f91c06dce0d51920cc4
anavidev <anavi.oliveira56@gmail.com>
2024-06-06 00:00:42 -0300

    principais-produtos

--- js/script_index.js
+++ js/script_index.js
@@ -1,36 +1,54 @@
-/* slideshow - corrigir */
-let sliders = document.querySelectorAll('#slide li');
-let foto_atual = 0;
-let qtd_fotos = sliders.length - 1;
+/* slide */
+const box = document.querySelector('.container-slide');
+const imagens = document.querySelectorAll('.imgSlide');
 
-function logicaSlides() {
-    let index = foto_atual ? foto_atual - 1 : qtd_fotos;
-    sliders[index].classList.remove('slider-active');
-    sliders[foto_atual].classList.add('slider-active');
-    foto_atual = foto_atual >= qtd_fotos ? 0 : foto_atual + 1;
+let contador = 0;
+
+function slider(){
+    contador++;
+
+    if(contador > imagens.length - 1){
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 500}px)`
 }
 
-window.setInterval(logicaSlides, 2000);
-
+setInterval(slider, 1500);
 
 /* evento mostrar principais produtos */
-const sessaoCards = document.querySelector('#sessaoCards')
+const sessaoCards = document.querySelector('#sessaoCards');
 
-const divCards = document.createElement('div')
-divCards.id = "divCards"
-sessaoCards.appendChild(divCards)
+const divCards = document.createElement('div');
+divCards.id = "divCards";
+sessaoCards.appendChild(divCards);
 
 let principais_produtos = [
-    {nome: "Pochete", descricao: "Pochete jeans com estampas."},
-    {nome: "Chaveiro", descricao: "Chaveiro jeans personalizado."},
-    {nome: "Bolsa", descricao: "Bolsa sintética com forro de bagum, impermeável para uso escolar ou de maquiagem."}
+    {nome: "Pochete", descricao: "Pochete jeans com estampas.", img: "../img/pochetes.jpeg"},
+    {nome: "Chaveiro", descricao: "Chaveiro jeans personalizado.", img: "../img/chaveiro.jpeg"},
+    {nome: "Bolsa", descricao: "Bolsa sintética com forro de bagum, impermeável para uso escolar ou de maquiagem.", img: "../img/sintetica.jpg"}
 ]
 
 for (let item of principais_produtos){
-    let divProduto = document.createElement('div')
-    divProduto.className = "produto"
-    divProduto.innerHTML = `${item.nome}<br>${item.descricao}`
-    divCards.appendChild(divProduto)
+    let divProduto = document.createElement('div');
+    divProduto.className = "produto";
+
+    let imgProduto = document.createElement('img');
+    imgProduto.src = item.img;
+    imgProduto.alt = item.nome;
+    imgProduto.className = "produto-imagem";
+    
+    let nomeProduto = document.createElement('h4');
+    nomeProduto.innerText = item.descricao;
+
+    let descricaoProduto = document.createElement('p');
+    descricaoProduto.innerText = item.descricao;
+
+    divProduto.appendChild(imgProduto);
+    divProduto.appendChild(nomeProduto);
+    divProduto.appendChild(descricaoProduto);
+    divCards.appendChild(divProduto);
+    
 }
 
 
