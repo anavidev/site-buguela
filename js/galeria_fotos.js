9f70ff930ca5261cdf786d8f23c5645085909ca6
anavidev <anavi.oliveira56@gmail.com>
2024-06-13 01:20:58 -0300

    correcao-imagens

--- js/galeria_fotos.js
+++ js/galeria_fotos.js
@@ -3,12 +3,12 @@ export default() => {
     let divGaleria = document.querySelector('#galeria-container')
 
     let fotos = [
-        {cod: 1, img: "../img/imagem1.webp", alt: "Imagem 1"},
-        {cod: 2, img: "../img/imagem2.jpeg", alt: "Imagem 2"},
-        {cod: 3, img: "../img/imagem3.jpg", alt: "Imagem 3"},
-        {cod: 4, img: "../img/imagem4.png", alt: "Imagem 4"},
-        {cod: 5, img: "../img/imagem5.jpg", alt: "Imagem 5"},
-        {cod: 6, img: "../img/imagem6.png", alt: "Imagem 6"},   
+        {cod: 1, img: "img/imagem1.webp", alt: "Imagem 1"},
+        {cod: 2, img: "img/imagem2.jpeg", alt: "Imagem 2"},
+        {cod: 3, img: "img/imagem3.jpg", alt: "Imagem 3"},
+        {cod: 4, img: "img/imagem4.png", alt: "Imagem 4"},
+        {cod: 5, img: "img/imagem5.jpg", alt: "Imagem 5"},
+        {cod: 6, img: "img/imagem6.png", alt: "Imagem 6"},   
     ];
 
     for (let item of fotos){
